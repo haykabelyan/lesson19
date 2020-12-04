dc6a180bfd431dc1facb273dfc8769c3a187acdb
haykabelyan <haykabelyan1@gmail.com>
2020-12-04 04:39:56 -0800

    First Commit

---
+++ lab1.js
@@ -0,0 +1,18 @@
+/*
+
+$pwd - print work drectory
+$ls - list
+$clear - 
+$cd - change directory
+$cd ..
+
+
+GITHUB
+git config --global user.name "haykabelyan"
+git config --global user.email "haykabelyan1@gmail.com"
+git config --list
+
+git init
+git status
+git add .
+git add lab1.js
